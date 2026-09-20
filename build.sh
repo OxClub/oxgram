911e15eff3bd957cae1a227cfc964fc19f3750fd
Oxclub <Oxclub@users.noreply.github.com>
2026-09-20 18:59:17 +0530

    update

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cd ~/downloads/oxgram
+git add .
+git commit -m "update"
+git push
+echo ""
+echo "✅ Sent to GitHub! Building now..."
+echo "👉 https://github.com/OxClub/oxgram/actions"
